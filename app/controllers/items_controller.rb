355d0ee03895897b94837e714ae49988442e5e44
syunsuke-I <odol@ymail.ne.jp>
2023-09-17 20:56:16 +0900

    rubocup対応

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
-  before_action :set_Item, only: %i[show edit update destroy]
+  before_action :set_item, only: %i[show]
 
   # GET /items
   def index
@@ -16,56 +16,16 @@ class ItemsController < ApplicationController
     @items_except_myself = items.reject { |item| item.id == @item.id }
   end
 
-  # これ以降は何も触っていません
-
-  # GET /items/new
-  def new
-    @item = Item.new
-  end
-
-  # GET /items/1/edit
-  def edit; end
-
-  # POST /items or /items.json
-  def create
-    @item = Item.new(permit_params)
-    @item.save
-    redirect_to @item
-  end
-
-  # PATCH/PUT /items/1 or /items/1.json
-  def update
-    respond_to do |format|
-      if @Item.update(Item_params)
-        format.html { redirect_to Item_url(@Item), notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @Item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @Item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /items/1 or /items/1.json
-  def destroy
-    @Item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_Item
-    @Item = Item.find(params[:id])
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def permit_params
-     pp :image
-     params.require(:item).permit(:name, :description, :image,:price)
+    Rails.logger.debug :image
+    params.require(:item).permit(:name, :description, :image, :price)
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+RSpec.describe Task do
   pending "add some examples to (or delete) #{__FILE__}"
 end
